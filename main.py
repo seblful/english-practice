446b6c93eaf31bb1254274a88d9a34d52856b493
seblful <alesha.chimba@gmail.com>
2026-02-11 00:09:05 +0300

    feat: enhance PDF cutting functionality in Typer CLI by adding section-based cutting options; implement page separation into units and exercises; improve PDFHandler methods for better page handling

--- main.py
+++ main.py
@@ -1,21 +1,82 @@
+from enum import Enum
+
 import typer
-from pathlib import Path
+
+from config.logging import get_logger
 from config.settings import settings
 from english_practice.extractors.pdf_handler import PDFHandler
+from english_practice.models import (
+    START_CONTENT_PAGE,
+    END_CONTENT_PAGE,
+    START_UNIT_PAGE,
+    END_UNIT_PAGE,
+    START_ANSWER_PAGE,
+    END_ANSWER_PAGE,
+)
+
+logger = get_logger(__name__)
 
 app = typer.Typer()
 
 
-@app.command(name="cut-pdf", help="Cut a PDF file from start page to end page.")
-def cut_pdf() -> None:
+class SectionType(str, Enum):
+    content = "content"
+    units = "units"
+    answers = "answers"
+
+
+@app.command(
+    name="cut-pdf",
+    help="Cut a PDF file based on the section type provided.",
+)
+def cut_pdf(
+    section: SectionType = typer.Argument(
+        ..., help="The section of the book to cut (content, units, answers)."
+    ),
+) -> None:
+
+    page_ranges = {
+        SectionType.content: (START_CONTENT_PAGE, END_CONTENT_PAGE),
+        SectionType.units: (START_UNIT_PAGE, END_UNIT_PAGE),
+        SectionType.answers: (START_ANSWER_PAGE, END_ANSWER_PAGE),
+    }
+
+    start_page, end_page = page_ranges[section]
+
+    logger.info(
+        f"Cutting PDF for section '{section.value}': Pages {start_page} to {end_page}"
+    )
+
     handler = PDFHandler()
+    output_path = (
+        settings.paths.snippets_dir
+        / settings.book.filename.split(".")[0]
+        / f"{section.value}.pdf"
+    )
+    output_path.parent.mkdir(parents=True, exist_ok=True)
     handler.cut_pdf(
-        file_path=settings.paths.books_dir / settings.book.original_file_name,
-        start_page=settings.book.start_page,
-        end_page=settings.book.end_page,
-        output_path=settings.paths.books_dir / settings.book.cut_file_name,
+        file_path=settings.paths.books_dir / settings.book.filename,
+        start_page=start_page,
+        end_page=end_page,
+        output_path=output_path,
+    )
+
+
+@app.command(
+    name="separate-page-images",
+    help="Separate pages from a PDF file into units pages and exercise pages.",
+)
+def separate_page_images() -> None:
+    handler = PDFHandler()
+    handler.separate_page_images(
+        file_path=settings.paths.books_dir / settings.book.filename,
+        start_page=settings.book.start_unit_page,
+        end_page=settings.book.end_unit_page,
+        units_pages_dir=settings.paths.units_pages_dir,
+        exercises_pages_dir=settings.paths.exercises_pages_dir,
+        dpi=settings.images.pages_dpi,
     )
 
 
 if __name__ == "__main__":
-    app()  # No need for a separate main() function or typer.run()
+    app()
--- src/english_practice/extractors/pdf_handler.py
+++ src/english_practice/extractors/pdf_handler.py
@@ -14,6 +14,30 @@ class PDFHandler:
         """Export pages from a PDF file from start page to end page."""
         with pymupdf.open(file_path) as pdf:
             with pymupdf.open() as new_pdf:
-                new_pdf.insert_pdf(pdf, from_page=start_page - 1, to_page=end_page)
+                new_pdf.insert_pdf(pdf, from_page=start_page - 1, to_page=end_page - 1)
                 new_pdf.save(output_path)
                 return output_path
+
+    def separate_page_images(
+        self,
+        file_path: Path,
+        start_page: int,
+        end_page: int,
+        units_pages_dir: Path,
+        exercises_pages_dir: Path,
+        dpi: int = 300,
+    ) -> None:
+        """Separate pages from a PDF file into units pages and exercise pages."""
+        with pymupdf.open(file_path) as pdf:
+            end_page = min(end_page, pdf.page_count)
+
+            unit_number = 1
+            for page_number in range(start_page - 1, end_page):
+                page = pdf.load_page(page_number)
+                pixmap = page.get_pixmap(dpi=dpi)
+
+                if page_number % 2 == 0:
+                    pixmap.save(units_pages_dir / f"{unit_number}.png")
+                    unit_number += 1
+                else:
+                    pixmap.save(exercises_pages_dir / f"{unit_number}.png")
